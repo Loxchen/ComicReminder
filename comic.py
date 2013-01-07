4000ba4f1f36ccea0cdf263cfbef3d32ee15f518
loxchen <loxchen@hotmail.com>
2013-01-07 22:49:54 +0800

    detect date and the latest comic
    read from a file with comic urls

--- comic.py
+++ comic.py
@@ -6,27 +6,34 @@ import re
 import sys
 
 def detect(url):
-  """detect the url"""
-  #f = urllib.urlopen(url)      
-  
-  path = os.path.abspath(os.path.join('.', 'comic.html'))
-  if os.path.exists(path):
-    f = open(path)
+    """detect the url"""
+    content = urllib.urlopen(url).read()
+    content = content.decode('gb2312')
+    match = re.search(u"\u66f4\u65b0\u65f6\u95f4：(\d+-\d+-\d+)", content)
+    match2 = re.search(u'<a href.* title="[\u4e00-\u9fa5]+(\d+)[\u4e00-\u9fa5]+" target="_blank" class="new">', content)
+    if match and match2:
+	return match.group(1) + u':\u7b2c' + match2.group(1) + u'\u8bdd'
+    
+def list(filename):
+    """read the comic list from file"""
+    comiclist = []
+    f = open(filename, 'rU')
     for line in f:
-      line = line.decode('gb2312')
-     # print line
-      match = re.search(u"(\u66f4\u65b0\u65f6\u95f4)：(\d+-\d+-\d+)", line)
-      if match:
-	print match.group(1)
-	print match.group(2)
+	comiclist.append(line.split(' '))
     f.close()
-  else:  
-    urlliburlretrieve(url, "comic.html")
-  #print f.read()
+    return comiclist
+
+def peek(filename):
+    """peek how are the comics"""
+    comiclist = list(filename)
+    for comic in comiclist:
+	print comic[0],
+	print detect(comic[1])
+	
 
 def main():
-  detect("http://imanhua.com/comic/1832/")
+    #detect("http://imanhua.com/comic/55/")
+    peek("comic_list.txt")
 
 if __name__ == '__main__':
-  main()
-  print sys.getdefaultencoding()
+    main()
